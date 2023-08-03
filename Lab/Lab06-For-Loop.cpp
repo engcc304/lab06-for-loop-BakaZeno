8162992fd76f6e3f22149803b1ab7a0d404a8796
BakaZeno <agarz3no@gmail.com>
2023-08-03 14:21:22 +0700

    hahahahah

--- Lab/Lab06-For-Loop.cpp
+++ Lab/Lab06-For-Loop.cpp
@@ -18,7 +18,21 @@
 #include <stdio.h>
 
 int main() {
-
+    int i=0;
+    int N;
+    printf("Enter value:\n");
+    scanf("%d",&N);
+    if(N%2 == 1){
+        for(i=1; i<=N; i+=2){
+        printf("%d ",i);
+        }
+    }
+    else{
+        for(i=N; i>=0; i-=2){
+        printf("%d ",i);
+        }
+    }
+    
     //--| YOUR CODE HERE
 
     return 0 ;
